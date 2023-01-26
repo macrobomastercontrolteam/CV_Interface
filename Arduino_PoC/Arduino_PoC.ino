93e1c12e4f53f5b82bc92c822cf187f7a072d90b
GJSK <Tim_GJSK@hotmail.com>
2023-01-26 18:43:33 -0500

    Init reader and handler state machines in PoC

--- Arduino_PoC/Arduino_PoC.ino
+++ Arduino_PoC/Arduino_PoC.ino
@@ -1,53 +1,181 @@
-#define MSG_SPECIAL_CHAR_STX 0x02
-#define MSG_SPECIAL_CHAR_ETX 0x03
-#define MSG_ACK_BIT 0x40
-#define MSG_TURN_ON_AUTO_AIM_MODE 0x01
-#define MSG_TURN_OFF 0x00
+#include <SoftwareSerial.h>  // SoftwareSerial
 
+/********* macro start *********/
+#define SESSION_TIMEOUT 5000
+#define DATA_PACKAGE_SIZE 7
+/********* macro end *********/
+
+/********* function declaration start *********/
+bool MsgReader_Heartbeat(bool *pfRxMsgComplete);
+void MsgHandler_Heartbeat(bool *pfMsgReceived, bool fStartCvSignal);
+/********* function declaration end *********/
+
+/********* typedefs start *********/
 typedef enum {
   ERROR_MSG = 0x00,
-  // Control hosted msgs
   MODE_CONTROL = 0x10,
-  // CV hosted msgs
   ENEMY_COORDINATE = 0x20,
-} eCv_MsgTypes;
+  ACK_MSG = 0x40,
+} eMsgTypes;
+
+typedef enum {
+  CHAR_STX = 0x02,
+  CHAR_ETX = 0x03,
+} eCharTypes;
+
+typedef enum {
+  TURN_OFF_MODE = 0x00,
+  TURN_ON_AUTO_AIM_MODE = 0x01,
+} eModeControl;
 
 typedef struct
 {
   uint16_t targetXCoordinate;
   uint16_t targetYCoordinate;
-} tCv_TargetCoordinateMsg;
+} tTargetCoordinateMsg;
 
-uint8_t rxBuffer[20];  //Initialized variable to store receive
-uint8_t txBuffer[10];  //Initialized variable to store receive
-uint8_t rxBufferWrittenSize = 0;
+typedef struct {
+  union {
+    struct {
+      const uint8_t bStx;  // always CHAR_STX
+      uint8_t bMsgType;
+      uint8_t abPayload[DATA_PACKAGE_SIZE - sizeof(uint8_t) * 3];
+      const uint8_t bEtx;  // always CHAR_ETX
+    };
+    char bData[DATA_PACKAGE_SIZE];
+  } uint8_t bUsedSize;
+} tMsgBuffer;
+/********* typedefs end *********/
 
-eCv_MsgTypes cvMsgHandler(uint8_t* rxBuffer);
+/********* module variable definitions start *********/
+// Serial is USB interface with RPi; used to flash Arduino and view real-time output
+// mySerial is UART interface with RPi; used to emulate CV communication
+const byte virtualRxPin = 6;
+const byte virtualTxPin = 7;
+SoftwareSerial mySerial(virtualRxPin, virtualTxPin);
+
+tMsgBuffer rxBuffer;
+tMsgBuffer txBuffer;
+/********* module variable definitions end *********/
 
 void setup() {
   Serial.begin(9600);
+  mySerial.begin(9600);
 }
 
 void loop() {
-  // read from STX to ETX
-  Serial.print("RECEIVER: ");
-  int bytesToRead = Serial.available();
-  int bytesToBuffer = min(bytesToRead, sizeof(rxBuffer) - rxBufferWrittenSize);
-  Serial.readBytes((char*)rxBuffer[rxBufferWrittenSize], bytesToBuffer);
-  rxBufferWrittenSize += bytesToBuffer;
-  if (bytesToRead != 0) {
-    Serial.println((char*)rxBuffer);
-  } else {
-    Serial.println();
-  }
-
-  // parse and handle
-  eCv_MsgTypes receivedMsgType = cvMsgHandler(rxBuffer);
-
+  static bool fRxMsgComplete = false;
+  MsgReader_Heartbeat(&fRxMsgComplete);
+  bool fStartCvSignal = true;  // mock user signal that enables auto-aim mode
+  MsgHandler_Heartbeat(&fRxMsgComplete, fStartCvSignal);
   delay(1000);
 }
 
-eCv_MsgTypes cvMsgHandler(uint8_t* rxBuffer) {
-  uint8_t index;
-  for (index = 0; index <)
+bool MsgReader_Heartbeat(bool *pfRxMsgComplete) {
+  if (*pfRxMsgComplete == false) {
+    // restart reading only after Rx Msg is processed, indicated by (*pfRxMsgComplete == false)
+    // read from STX to ETX
+    if ((mySerial.readBytes(rxBuffer, 1) == 1) && (rxBuffer[0] == CHAR_STX)) {
+      rxBufferSize = 1;
+      while ((rxBufferSize < sizeof(rxBuffer)) && mySerial.readBytes(&rxBuffer[rxBufferSize], 1) == 1) {
+        if (rxBuffer[rxBufferSize] == CHAR_ETX) {
+          *pfRxMsgComplete = true;
+          // echo to scanner
+          mySerial.write(rxBuffer, rxBufferSize);
+          rxBufferSize = 0;
+          break;
+        } else if (rxBuffer[rxBufferSize] == CHAR_STX) {
+          // restart reading from STX
+          rxBufferSize = 1;
+        } else {
+          rxBufferSize++;
+        }
+      }
+    }
+  }
+  return *pfRxMsgComplete;
+}
+
+void MsgHandler_Heartbeat(bool *pfRxMsgComplete, bool fStartCvSignal) {
+  enum {
+    HANDLER_IDLE,
+    HANDLER_ENABLE_CV,
+    HANDLER_WAIT_FOR_COORDINATE,
+    HANDLER_DISABLE_CV,
+    HANDLER_WAIT_FOR_ACK,
+  } tHandlerState;
+  static tHandlerState HandlerState = HANDLER_IDLE;
+  static tHandlerState NextHandlerState = HANDLER_IDLE;
+  static uint32_t ulOldRxTimestamp = 0;
+  uint32_t ulNewRxTimestamp = millis();
+  bool fMsgProcessed = false;
+
+  // global user interrupt
+  if (fStartCvSignal == false) {
+    HandlerState = HANDLER_DISABLE_CV;
+  }
+
+  txBufferSize = 0;
+
+  switch (HandlerState) {
+    case HANDLER_IDLE:
+      {
+        if (fStartCvSignal) {
+          HandlerState = HANDLER_ENABLE_CV;
+        }
+        break;
+      }
+    case HANDLER_ENABLE_CV:
+      {
+        if ((unsigned long)(ulNewRxTimestamp - ulOldRxTimestamp) > SESSION_TIMEOUT) {  // works even with overflow
+          HandlerState = HANDLER_DISABLE_CV;
+        } else {
+          txBuffer[txBufferSize++] = CHAR_STX;
+          txBuffer[txBufferSize++] = MODE_CONTROL;
+          txBuffer[txBufferSize++] = TURN_ON_AUTO_AIM_MODE;
+          txBuffer[txBufferSize++] = CHAR_ETX;
+          HandlerState = HANDLER_WAIT_FOR_ACK;
+          NextHandlerState = HANDLER_WAIT_FOR_COORDINATE;
+        }
+        break;
+      }
+    case HANDLER_WAIT_FOR_COORDINATE:
+      {
+        if ((unsigned long)(ulNewRxTimestamp - ulOldRxTimestamp) > SESSION_TIMEOUT) {  // works even with overflow
+          HandlerState = HANDLER_DISABLE_CV;
+        } else {
+        }
+        break;
+      }
+    case HANDLER_DISABLE_CV:
+      {
+        HandlerState = HANDLER_ENABLE_CV;
+        break;
+      }
+    case HANDLER_WAIT_FOR_ACK:
+      {
+        if ((unsigned long)(ulNewRxTimestamp - ulOldRxTimestamp) > SESSION_TIMEOUT) {  // works even with overflow
+          HandlerState = HANDLER_DISABLE_CV;
+        } else if (*pfRxMsgComplete) {
+          if (rxBuffer[1] == ACK_MSG) {
+            HandlerState = NextHandlerState;
+          }
+          fMsgProcessed = true;
+        }
+        break;
+      }
+  }
+
+  if (*pfRxMsgComplete) {
+    ulOldRxTimestamp = ulNewRxTimestamp;
+  }
+
+  // must check after timestamp is refreshed
+  if (fMsgProcessed) {
+    *pfRxMsgComplete = false;
+  }
+
+  if (txBufferSize > 0) {
+    mySerial.write(txBuffer, txBufferSize);
+  }
 }
