beb0ac6a887c23ff003197d654f5a15edce0d91c
Bill <56321690+Bill-Haoyu-Lin@users.noreply.github.com>
2023-03-28 22:27:28 -0400

    Update CvCmdApi.py

--- CvCmdApi.py
+++ CvCmdApi.py
@@ -53,6 +53,8 @@ class CvCmdHandler:
     # @param[in] gimbal_coordinate_x and gimbal_coordinate_y: type is int; must be positive; will be converted to uint16_t
     # @param[in] chassis_speed_x and chassis_speed_y: type is float; can be positive/negative; will be converted to float (32 bits)
     def CvCmd_Heartbeat(self, gimbal_coordinate_x, gimbal_coordinate_y, chassis_speed_x, chassis_speed_y):
+        
+        #switching x and y axis for gimbal and chassic to fit RM code 
         gimbal_coordinate_x, gimbal_coordinate_y = gimbal_coordinate_y, gimbal_coordinate_x
         chassis_speed_x, chassis_speed_y = chassis_speed_y, chassis_speed_x
         # Tx
