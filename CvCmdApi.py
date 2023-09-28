4aad08fca8bcccc6469be15b37a38ae8653a3550
GJSK-Novice <Tim_GJSK@hotmail.com>
2023-09-28 16:54:16 -0400

    Implemented Timestamp sync process

--- CvCmdApi.py
+++ CvCmdApi.py
@@ -5,11 +5,14 @@ import struct
 import re
 import math
 
+
 class CvCmdHandler:
     # misc constants
-    DATA_PACKAGE_SIZE = 19  # 2 bytes header, 1 byte msg type, 16 bytes payload
-    DATA_PAYLOAD_INDEX = 3
+    DATA_TIMESTAMP_INDEX = 2
+    DATA_PACKAGE_SIZE = 21  # 2 bytes header, 1 byte msg type, 16 bytes payload
+    DATA_PAYLOAD_INDEX = 5
     MIN_TX_SEPARATION_SEC = 0  # reserved for future, currently control board is fast enough
+    MIN_INFO_REQ_SEPARATION_SEC = 1
     SHOOT_TIMEOUT_SEC = 2
     DEBUG_CV = True
 
@@ -17,6 +20,8 @@ class CvCmdHandler:
         MSG_MODE_CONTROL = b'\x10'
         MSG_CV_CMD = b'\x20'
         MSG_ACK = b'\x40'
+        MSG_INFO_REQUEST = b'\x50'
+        MSG_INFO_FEEDBACK = b'\x51'
 
     class eSepChar(Enum):  # start and ending hexes, acknowledgement bit
         CHAR_HEADER = b'>>'
@@ -33,6 +38,10 @@ class CvCmdHandler:
         MODE_AUTO_MOVE_BIT = 0b00000010
         MODE_ENEMY_DETECTED_BIT = 0b00000100
         MODE_SHOOT_BIT = 0b00001000
+
+    class eInfoBits(Enum):
+        MODE_TRAN_DELTA_BIT = 0b00000001
+        MODE_CV_SYNC_TIME_BIT = 0b00000010
 
     # @param[in] max_camera_coordinate_x, max_camera_coordinate_y: type is int; unit is pixel; starts from 1
     # @param[in] max_camera_angle_yaw: field of view angle; type is float; unit is radian
@@ -51,23 +60,63 @@ class CvCmdHandler:
         #     else:
         #         raise NotImplementedError
 
+        self.ackMsgInfo = {"reqCtrlTimestamp": -1, "reqRxTimestamp": -1}
+        self.CvSyncTime = 0
         self.CvCmd_Reset()
 
         # self.ser = serial.Serial(port='/dev/ttyTHS2', baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
         # Manual test on Windows
         self.ser = serial.Serial(port=serial_port, baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
-        self.txCvCmdMsg = bytearray(self.eSepChar.CHAR_HEADER.value + self.eMsgType.MSG_CV_CMD.value + self.eSepChar.CHAR_UNUSED.value*16)
-        self.txSetModeMsg = bytearray(self.eSepChar.CHAR_HEADER.value + self.eMsgType.MSG_MODE_CONTROL.value + b'\x00' + self.eSepChar.CHAR_UNUSED.value*15)
-        # txAckMsg is always the same, so use the immutable bytes object
-        self.txAckMsg = b''.join([self.eSepChar.CHAR_HEADER.value, self.eMsgType.MSG_ACK.value, self.eSepChar.ACK_ASCII.value, self.eSepChar.CHAR_UNUSED.value*13])
+        self.txCvCmdMsg = self.CvCmd_InitTxMsg(self.eMsgType.MSG_CV_CMD.value)
+        self.txInfoRequestMsg = self.CvCmd_InitTxMsg(self.eMsgType.MSG_INFO_REQUEST.value)
+        self.txSetModeMsg = self.CvCmd_InitTxMsg(self.eMsgType.MSG_MODE_CONTROL.value)
+        self.txAckMsg = self.CvCmd_InitTxMsg(self.eMsgType.MSG_ACK.value + self.eSepChar.ACK_ASCII.value)
 
-        assert (len(self.txCvCmdMsg) == self.DATA_PACKAGE_SIZE)
-        assert (len(self.txAckMsg) == self.DATA_PACKAGE_SIZE)
-        assert (len(self.txSetModeMsg) == self.DATA_PACKAGE_SIZE)
+        # @TODO: reserve data placeholders within msg frames for each type of msg beforehand
+        self.txAckMsgPayloadIndex = self.DATA_PAYLOAD_INDEX + len(self.eSepChar.ACK_ASCII.value)
+
+    def CvCmd_InitTxMsg(self, payload):
+        txMsg = bytearray(self.eSepChar.CHAR_HEADER.value)
+        txMsg += b'\x00\x00'  # placeholder for timestamp
+        txMsg += payload
+        txMsg += self.eSepChar.CHAR_UNUSED.value*(self.DATA_PACKAGE_SIZE-len(txMsg))
+        return txMsg
+
+    def CvCmd_GetUint16Time(self):
+        return int(time.time()*1000) & ((1 << 16)-1)
+
+    def CvCmd_GetUint16Delta(self, minuendTime, subtrahendTime):
+        signedDelta = minuendTime - subtrahendTime
+        unsignedDelta = signedDelta if (signedDelta >= 0) else (signedDelta + (1 << 16))
+        return unsignedDelta
+
+    def CvCmd_BuildSendTxMsg(self, txMsg):
+        txMsg[self.DATA_TIMESTAMP_INDEX:self.DATA_TIMESTAMP_INDEX+2] = struct.pack('<H', self.CvCmd_GetUint16Delta(self.CvCmd_GetUint16Time(), self.CvSyncTime))
+        self.ser.write(txMsg)
+        self.prevTxTime = time.time()
+        if self.DEBUG_CV:
+            assert (len(txMsg) == self.DATA_PACKAGE_SIZE)
+            print("Sent: ", txMsg)
+            if txMsg is self.txAckMsg:
+                print("ACK sent")
+            elif txMsg is self.txInfoRequestMsg:
+                print("Info request sent")
+            elif txMsg is self.txCvCmdMsg:
+                print("Gimbal cmd sent")
+            elif txMsg is self.txSetModeMsg:
+                print("Shoot req sent")
 
     def CvCmd_Reset(self):
+        # reqCtrlTimestamp: control native timestamp contained within the request sent by control board
+        # reqRxTimestamp: receive timestamp of the request sent by control board
+        self.ackMsgInfo["reqCtrlTimestamp"] = -1
+        self.ackMsgInfo["reqRxTimestamp"] = -1
+        self.infoRequestPending = 0
         self.prevTxTime = 0
+        self.prevInfoReqTime = 0
+        self.cvCmdCount = 0
+        self.tranDelta = None  # Transmission delay time in ms
         self.AutoAimSwitch = False
         self.AutoMoveSwitch = False
         self.EnemySwitch = False
@@ -126,7 +175,7 @@ class CvCmdHandler:
             self.gimbal_cmd_delta_yaw = math.atan(self.target_depth*math.sin(camera_angle_x)/(self.camera_to_axis_distance+math.sin(camera_angle_x)))
             self.gimbal_cmd_delta_pitch = math.atan(self.target_depth*math.sin(camera_angle_y)/(self.camera_to_axis_distance+math.sin(camera_angle_y)))
         if self.DEBUG_CV:
-            print("gimbal_cmd_delta_yaw: ",self.gimbal_cmd_delta_yaw,"gimbal_cmd_delta_pitch: ",self.gimbal_cmd_delta_pitch)
+            print("gimbal_cmd_delta_yaw: ", self.gimbal_cmd_delta_yaw, "gimbal_cmd_delta_pitch: ", self.gimbal_cmd_delta_pitch)
 
         # Chassis: speed to speed conversion
         # CV positive directions: +x is to the right, +y is upwards
@@ -152,34 +201,60 @@ class CvCmdHandler:
             fHeartbeatFinished = True
 
         elif self.Rx_State == self.eRxState.RX_STATE_WAIT_FOR_PKG:
-            # polling for control msg, if any msg received, ACK back
             if self.ser.in_waiting >= self.DATA_PACKAGE_SIZE:
                 bytesRead = self.ser.read(self.ser.in_waiting)
-                dataPackets = re.findall(self.eSepChar.CHAR_HEADER.value + self.eMsgType.MSG_MODE_CONTROL.value + b"." + self.eSepChar.CHAR_UNUSED.value + b"{15}", bytesRead)
+                # @TODO: use regex to search msg by msg instead of processing only the last msg. For now, control board don't have much to send, so it's fine.
+                setModeRequestPackets = re.findall(self.eSepChar.CHAR_HEADER.value + b".." + self.eMsgType.MSG_MODE_CONTROL.value + b"." + self.eSepChar.CHAR_UNUSED.value + b"{15}", bytesRead)
+                infoFeedbackPackets = re.findall(self.eSepChar.CHAR_HEADER.value + b".." + self.eMsgType.MSG_INFO_FEEDBACK.value + b"." + b".." + self.eSepChar.CHAR_UNUSED.value + b"{13}", bytesRead)
                 if self.DEBUG_CV:
                     print("bytesRead: ", bytesRead)
-                    print("dataPackets: ", dataPackets)
+                    print("setModeRequestPackets: ", setModeRequestPackets)
+                    print("infoFeedbackPackets: ", infoFeedbackPackets)
 
-                if dataPackets:
+                if setModeRequestPackets:
                     # read the mode of the last packet, because it's the latest
-                    rxSwitchBuffer = dataPackets[-1][self.DATA_PAYLOAD_INDEX]
+                    rxSwitchBuffer = setModeRequestPackets[-1][self.DATA_PAYLOAD_INDEX]
                     self.AutoAimSwitch = bool(rxSwitchBuffer & self.eModeControlBits.MODE_AUTO_AIM_BIT.value)
                     self.AutoMoveSwitch = bool(rxSwitchBuffer & self.eModeControlBits.MODE_AUTO_MOVE_BIT.value)
                     self.EnemySwitch = bool(rxSwitchBuffer & self.eModeControlBits.MODE_ENEMY_DETECTED_BIT.value)
                     # Shoot switch is controlled by CV
                     # self.ShootSwitch = bool(rxSwitchBuffer & self.eModeControlBits.MODE_SHOOT_BIT.value)
+                    self.ackMsgInfo["reqCtrlTimestamp"] = struct.unpack('<H', setModeRequestPackets[-1][self.DATA_TIMESTAMP_INDEX:self.DATA_TIMESTAMP_INDEX+2])[0]
+                    self.ackMsgInfo["reqRxTimestamp"] = self.CvCmd_GetUint16Time()
+                    self.cvCmdCount = 0
                     self.Rx_State = self.eRxState.RX_STATE_SEND_ACK
                     fHeartbeatFinished = False
-                else:
+
+                if infoFeedbackPackets:
+                    for packet in infoFeedbackPackets:
+                        rxInfoType = packet[self.DATA_PAYLOAD_INDEX]
+                        rxInfoData = packet[self.DATA_PAYLOAD_INDEX+1:self.DATA_PAYLOAD_INDEX+3]
+                        if rxInfoType == self.eInfoBits.MODE_TRAN_DELTA_BIT.value:
+                            # Warning: tranDelta is signed
+                            self.tranDelta = struct.unpack('<h', rxInfoData)[0]
+                            self.infoRequestPending &= ~rxInfoType
+                            if self.DEBUG_CV:
+                                print("tranDelta: ", self.tranDelta)
+                        elif rxInfoType == self.eInfoBits.MODE_CV_SYNC_TIME_BIT.value:
+                            self.CvSyncTime = struct.unpack('<H', rxInfoData)[0]
+                            self.infoRequestPending &= ~rxInfoType
+                            if self.DEBUG_CV:
+                                print("CvSyncTime: ", self.CvSyncTime)
+                    # Do not change Rx_State or fHeartbeatFinished
+
+                # No valid msg received, retry connection
+                if not (setModeRequestPackets or infoFeedbackPackets):
                     self.ser.reset_input_buffer()
                     fHeartbeatFinished = True
 
         elif self.Rx_State == self.eRxState.RX_STATE_SEND_ACK:
             if time.time() - self.prevTxTime > self.MIN_TX_SEPARATION_SEC:
-                self.ser.write(self.txAckMsg)
-                if self.DEBUG_CV:
-                    print("ACK sent")
-                self.prevTxTime = time.time()
+                self.CvSyncTime = self.CvCmd_GetUint16Time()
+                uiExecDelta = self.CvCmd_GetUint16Delta(self.CvSyncTime, self.ackMsgInfo["reqRxTimestamp"])
+                self.txAckMsg[self.txAckMsgPayloadIndex:self.txAckMsgPayloadIndex+6] = struct.pack('<HHH', self.ackMsgInfo["reqCtrlTimestamp"], uiExecDelta, self.CvSyncTime)
+                self.CvCmd_BuildSendTxMsg(self.txAckMsg)
+                # Trigger info request in TxHeartbeat
+                self.infoRequestPending |= self.eInfoBits.MODE_TRAN_DELTA_BIT.value | self.eInfoBits.MODE_CV_SYNC_TIME_BIT.value
                 self.Rx_State = self.eRxState.RX_STATE_WAIT_FOR_PKG
             fHeartbeatFinished = True
 
@@ -187,20 +262,24 @@ class CvCmdHandler:
 
     def CvCmd_TxHeartbeat(self):
         # Tx: keeping sending cmd to keep control board alive (watchdog timer logic)
-        if (self.AutoAimSwitch or self.AutoMoveSwitch) and (time.time() - self.prevTxTime > self.MIN_TX_SEPARATION_SEC):
-            self.txCvCmdMsg[self.DATA_PAYLOAD_INDEX:self.DATA_PAYLOAD_INDEX+16] = struct.pack('<ffff', self.gimbal_cmd_delta_yaw, self.gimbal_cmd_delta_pitch, self.chassis_cmd_speed_x, self.chassis_cmd_speed_y)
-            if self.DEBUG_CV:
-                print("gimbal cmd sent")
-            self.ser.write(self.txCvCmdMsg)
-            self.prevTxTime = time.time()
-        
+        if (time.time() - self.prevTxTime > self.MIN_TX_SEPARATION_SEC):
+            if self.infoRequestPending and (time.time() - self.prevInfoReqTime > self.MIN_INFO_REQ_SEPARATION_SEC):
+                self.txInfoRequestMsg[self.DATA_PAYLOAD_INDEX] = self.infoRequestPending
+                self.CvCmd_BuildSendTxMsg(self.txInfoRequestMsg)
+                self.prevInfoReqTime = time.time()
+            elif ((self.AutoAimSwitch or self.AutoMoveSwitch) and (self.tranDelta != None)):
+                self.txCvCmdMsg[self.DATA_PAYLOAD_INDEX:self.DATA_PAYLOAD_INDEX+16] = struct.pack('<ffff', self.gimbal_cmd_delta_yaw, self.gimbal_cmd_delta_pitch, self.chassis_cmd_speed_x, self.chassis_cmd_speed_y)
+                self.CvCmd_BuildSendTxMsg(self.txCvCmdMsg)
+                self.cvCmdCount += 1
+                if (self.cvCmdCount % 10 == 0):
+                    self.infoRequestPending |= self.eInfoBits.MODE_TRAN_DELTA_BIT.value
+
         # Latching shoot switch logic
         if self.ShootSwitch:
             if self.PrevShootSwitch == False:
                 if time.time() - self.prevTxTime > self.MIN_TX_SEPARATION_SEC:
                     self.txSetModeMsg[self.DATA_PAYLOAD_INDEX] = self.eModeControlBits.MODE_SHOOT_BIT.value
-                    self.ser.write(self.txSetModeMsg)
-                    self.prevTxTime = time.time()
+                    self.CvCmd_BuildSendTxMsg(self.txSetModeMsg)
                     self.shootStartTime = time.time()
                     self.PrevShootSwitch = True
             elif time.time() - self.shootStartTime > self.SHOOT_TIMEOUT_SEC:
--- README.md
+++ README.md
@@ -28,11 +28,10 @@ Typically, we fix control board to the same orientation as the cannon, so omit t
 
 ## Sequence Diagrams
 ### Normal CV command process
-Notes:
+- Unit of all timestamps is ms, format is uint16_t. It's defined by timestamp = raw_timestamp - sync_time. if raw_timestamp overflow and becomes smaller than sync_time, timestamp = raw_timestamp - sync_time **+ 0x10000** to avoid negative number. Exception are TranDelta and TranDelta_MA, which are int16_t, because it may become negative when control board's tick updates slower than CV.
 - Reason for CV to request for CvSyncTime at around the 8th step:<br/>
 CV may send multiple ACKs to multiple set-mode requests that are stuck in the buffer before CV starts processing. CV will memorize the CvSyncTime of the last ACK, while control board may not, since some messages may be lost caused by turn around time of DMA controller.<br/>
-Therefore, to correlate, control board must have 
-- Take special care to underflow and overflow situations of our uint16_t timestamp. For example, in C language, **(uint16_t)** (time2-time1) would return the same result when time2 overflow as the case where there isn't. This C program behaviour is ideal for us.
+Therefore, to correlate, control board must have latest CvSyncTime stored and sent to CV when requested.
 - TranDelta history should be cleared every time Control board boots up.
 - For consistency, all timestamps within package headers should be synchronized, i.e. offseted by SyncTime, even before the synchronization process, where . For example, for control board, the timestamp should be CtrlTimestamp(Tx) - CtrlSyncTime. For CV, the timestamp should be CvTimestamp(Tx) - CvSyncTime.
 - For all msgs control receives from CV, update TranDelta_MA and its history. Newest TranDelta = CtrlTimestamp(Rx) - CtrlSyncTime - MsgTimestamp, except for the ACK msg where calculation is different as illustrated in the diagram below.
@@ -45,31 +44,39 @@ sequenceDiagram
             Control->>CV :Set-mode request: Auto-aim or Auto-move on<br/>Including ReqTime = CtrlTimestamp(Tx) - CtrlSyncTime
             break when ACK is received
                 activate CV
-                CV-->CV :CvSyncTime = CvTimestamp(Tx)<br/>ExecDelta = CvTimestamp(Tx) - CvTimestamp(Rx)
+                CV-->CV :CvSyncTime = CvTimestamp(Tx)<br/>ExecDelta = CvSyncTime - CvTimestamp(Rx)
                 deactivate CV
-                CV->>Control :ACK (with CvSyncTime, ExecDelta and ReqTime)
-                Control-->Control: TranDelta = (CtrlTimestamp(Rx) - CtrlSyncTime - ReqTime - ExecDelta)/2<br/>TranDelta_MA = MovingAverage(TranDelta)<br/>CtrlSyncTime = CtrlTimestamp(Rx) - TranDelta_MA<br/>Save CvSyncTime
+                CV->>Control :ACK<br/>Including Timestamp = CvTimestamp(Tx) - CvSyncTime = 0<br/>Payload: CvSyncTime, ExecDelta and ReqTime
+                Control-->Control: TranDelta = (CtrlTimestamp(Rx) - CtrlSyncTime - ReqTime - ExecDelta)/2<br/>TranDelta_MA = MovingAverage(TranDelta)<br/>CtrlSyncTime = CtrlTimestamp(Rx) - TranDelta<br/>Save CvSyncTime
             end
         end
 
-        loop if CV responds to all the set-mode requests stuck in the buffer before it starts processing
+        opt if CV responds to all the set-mode requests stuck in the buffer before it starts processing
             activate CV
-            CV-->CV :CvSyncTime = CvTimestamp(Tx)<br/>ExecDelta = CvTimestamp(Tx) - CvTimestamp(Rx)
+            CV-->CV :CvSyncTime = CvTimestamp(Tx)<br/>ExecDelta = CvSyncTime - CvTimestamp(Rx)
             deactivate CV
-            CV->>Control :ACK (with CvSyncTime, ExecDelta and ReqTime)
-            Control-->Control: TranDelta = (CtrlTimestamp(Rx) - CtrlSyncTime - ReqTime - ExecDelta)/2<br/>TranDelta_MA = MovingAverage(TranDelta)<br/>CtrlSyncTime = CtrlTimestamp(Rx) - TranDelta_MA<br/>Save CvSyncTime
+            CV->>Control :ACK<br/>Including Timestamp = CvTimestamp(Tx) - CvSyncTime = 0<br/>Payload: CvSyncTime, ExecDelta and ReqTime
+            Control-->Control: TranDelta = (CtrlTimestamp(Rx) - CtrlSyncTime - ReqTime - ExecDelta)/2<br/>TranDelta_MA = MovingAverage(TranDelta)<br/>CtrlSyncTime = CtrlTimestamp(Rx) - TranDelta<br/>Save CvSyncTime
         end
 
-        CV->>Control :Request Info:TranDelta & CvSyncTime
-        Control-->Control :Update TranDelta and TranDelta_MA
-        Control->>CV :Info:TranDelta_MA
-        Control->>CV :Info:CvSyncTime
+        loop
+            CV->>Control :Request Info:TranDelta & CvSyncTime
+            Control-->Control :Update TranDelta and TranDelta_MA
+            break when feedbacks for all requested info are received
+                Control->>CV :Info:TranDelta_MA
+                Control->>CV :Info:CvSyncTime
+            end
+        end
 
         Note left of Control: Cv Command Control Board
         loop every MIN_TX_SEPARATION_SEC<br/>until CV msg interval > 1 sec,<br/>for which Ctrl would think CV is offline
             CV->>Control :CvCmdMsg<br/>(Enemy 3D location in Camera frame)<br/>(chassis 2D target location in Camera frame)
-            Control-->Control :Update TranDelta and TranDelta_MA
-            opt Every 5 seconds
+            par
+                Control-->Control :Update TranDelta and TranDelta_MA
+            and
+                CV-->CV :Wait for MIN_TX_SEPARATION_SEC
+            end
+            opt Every 500 iterations
                 CV->>Control :Request info:TranDelta
                 Control-->Control :Update TranDelta and TranDelta_MA
                 Control->>CV :Info:TranDelta
@@ -78,7 +85,7 @@ sequenceDiagram
         end
     end
 ```
-### Gimbal frame calibration process (not in used right now)
+### Calibration process for transformation matrix between camera frame and control board IMU frame<br/>(not in use right now)
 ``` mermaid
 sequenceDiagram
     Control-->Control:Calibration process started by remote controller
