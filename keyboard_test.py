04119013fce27e4d9ce70171495cda157d04cf01
GJSK-Novice <Tim_GJSK@hotmail.com>
2024-03-14 02:05:18 -0400

    Added gimbal test

--- keyboard_test.py
+++ keyboard_test.py
@@ -12,32 +12,43 @@ CvCmder = CvCmdApi.CvCmdHandler('COM19', 1280, 720, 100)
 import keyboard
 import time
 oldflags = (False, False, False)
-chassis_speed_y_Slider = 0
-chassis_speed_x_Slider = 0
-# gimbal_coordinate_y_Slider_arr = [100, 0]
-gimbal_coordinate_y_Slider = 0
-gimbal_coordinate_y_Slider_index = 0
 counter = 0
 loop_delay = 0.05
 while True:
+    chassis_vy = 0
+    chassis_vx = 0
+    gimbal_pitch = 0
+    gimbal_yaw = 0
+
     if keyboard.is_pressed('s'):
-        chassis_speed_y_Slider += -0.02
+        chassis_vy = -0.3
     elif keyboard.is_pressed('w'):
-        chassis_speed_y_Slider += 0.02
+        chassis_vy = 0.3
     elif keyboard.is_pressed('a'):
-        chassis_speed_x_Slider += -0.02
+        chassis_vx = -0.3
     elif keyboard.is_pressed('d'):
-        chassis_speed_x_Slider += 0.02
+        chassis_vx = 0.3
+
+    # percentage
+    if keyboard.is_pressed('k'):
+        gimbal_pitch = -0.1
+    elif keyboard.is_pressed('i'):
+        gimbal_pitch = 0.1
+    elif keyboard.is_pressed('j'):
+        gimbal_yaw = -0.1
+    elif keyboard.is_pressed('l'):
+        gimbal_yaw = 0.1
+
     if keyboard.is_pressed('q'):
         break
     
-    if counter == int(5/loop_delay):
-        CvCmder.CvCmd_Shoot()
+    # if counter == int(5/loop_delay):
+    #     CvCmder.CvCmd_Shoot()
 
-    flags = CvCmder.CvCmd_Heartbeat(gimbal_coordinate_x=1280/2, gimbal_coordinate_y=720/2,chassis_speed_x=chassis_speed_x_Slider,chassis_speed_y=chassis_speed_y_Slider)
+    flags = CvCmder.CvCmd_Heartbeat(gimbal_coordinate_x=1280/2*(1+gimbal_yaw), gimbal_coordinate_y=720/2*(1+gimbal_pitch),chassis_speed_x=chassis_vx,chassis_speed_y=chassis_vy)
     if flags != oldflags:
         oldflags = flags
         print(flags)
-    print(chassis_speed_x_Slider, chassis_speed_y_Slider)
+    # print(chassis_vx, chassis_vy)
     time.sleep(loop_delay)
     counter += 1
