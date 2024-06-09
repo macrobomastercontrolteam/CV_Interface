1eb1f5194a3ea924dd4cb67550ceaec69500e75f
GJSK-Novice <Tim_GJSK@hotmail.com>
2024-06-09 05:16:18 -0400

    Fixed keyboard delay

--- keyboard_test.py
+++ keyboard_test.py
@@ -1,19 +1,9 @@
 import CvCmdApi
-
-# Example usage
-CvCmder = CvCmdApi.CvCmdHandler('COM27')
-# oldflags = (False, False, False)
-# while True:
-#     flags = CvCmder.CvCmd_Heartbeat(0, 0, 0, 0)  # gimbal_coordinate_x, gimbal_coordinate_y, chassis_speed_x, chassis_speed_y
-#     if flags != oldflags:
-#         oldflags = flags
-#         print(flags)
+CvCmder = CvCmdApi.CvCmdHandler('COM16')
 
 import keyboard
 import time
-oldflags = (False, False, False)
-counter = 0
-loop_delay = 0.05
+loop_delay = 0
 gimbal_pitch = 0
 gimbal_yaw = 0
 while True:
@@ -31,25 +21,20 @@ while True:
 
     # percentage
     if keyboard.is_pressed('k'):
-        gimbal_pitch = -0.2
+        gimbal_pitch = 0.5
     elif keyboard.is_pressed('i'):
-        gimbal_pitch = 0.2
+        gimbal_pitch = -0.5
     elif keyboard.is_pressed('j'):
-        gimbal_yaw = -0.2
+        gimbal_yaw = -0.3
     elif keyboard.is_pressed('l'):
-        gimbal_yaw = 0.2
+        gimbal_yaw = 0.3
 
     if keyboard.is_pressed('q'):
         break
     
-    # if counter == int(5/loop_delay):
-    #     CvCmder.CvCmd_Shoot()
+    if keyboard.is_pressed('g'):
+        CvCmder.CvCmd_Shoot()
 
-    flags = CvCmder.CvCmd_Heartbeat(gimbal_pitch_target=gimbal_pitch, gimbal_yaw_target=gimbal_yaw, chassis_speed_x=chassis_vx, chassis_speed_y=chassis_vy)
-    # flags = CvCmder.CvCmd_Heartbeat(gimbal_coordinate_x=1280/2*(1+gimbal_yaw), gimbal_coordinate_y=720/2*(1+gimbal_pitch),chassis_speed_x=chassis_vx,chassis_speed_y=chassis_vy)
-    if flags != oldflags:
-        oldflags = flags
-        print(flags)
-    # print(chassis_vx, chassis_vy)
+    CvCmder.CvCmd_Heartbeat(gimbal_pitch_target=gimbal_pitch, gimbal_yaw_target=gimbal_yaw, chassis_speed_x=chassis_vx, chassis_speed_y=chassis_vy)
+    # Warning: for python version less than 3.11 running on Windows, the min achieveable sleep time is around 15ms. Test yourself before using time.sleep()
     time.sleep(loop_delay)
-    counter += 1
